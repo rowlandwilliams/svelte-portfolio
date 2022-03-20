c150f7be42100146e1bbf315d9caf54ff7f97529
rowlandwilliams <rowland.williams.94@gmail.com>
2022-03-20 14:50:58 +0000

    add icon component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+	import RwIcon from './RwIcon/RwIcon.svelte';
+</script>
+
+<div class="mb-8 flex items-center gap-x-2">
+	<RwIcon />
+	<h1 class="font-medium text-gray-900">Rowland Williams</h1>
+</div>
